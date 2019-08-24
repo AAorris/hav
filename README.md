4d97e49bb0914b8d2c4b80461781ad995d4a2aed
Aaron Morris <aaorris@live.com>
2019-08-24 13:27:30 -0700

    Allow selection by tag name, name, class name, and more default argument handling.

--- README.md
+++ README.md
@@ -28,4 +28,13 @@ https://www.windfinder.com/forecast/jericho_beach_park
 # Open a#wind.href on a mac
 > curl https://amorris.ca/launch | hav wind href | xargs open
 https://www.windfinder.com/forecast/jericho_beach_park
+
+curl https://hav.amorris.ca/ | hav .header
+curl https://hav.amorris.ca/ | hav .pre text
+curl https://hav.amorris.ca/ | hav ^h2 name
+curl https://hav.amorris.ca/ | hav ~why text
+curl https://hav.amorris.ca/ | hav ~usage text
+curl https://amorris.ca/launch/ | hav ^a href
+curl https://amorris.ca/launch/ | hav ^a id
+curl https://amorris.ca/launch/ | hav music href
 ```
--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 #!/usr/bin/env node
 const stdin = require('get-stdin');
 const DomParser = require('dom-parser');
-const args = require('yargs').demandCommand(2, 'Requires: <id> <attribute>').argv;
+const args = require('yargs').demandCommand(1, 'Requires: <id> <attribute>').argv;
+const getValue = attr => element => {
+  if (element === null) return
+  switch (attr || 'outerhtml') {
+    case 'html':
+      return element.innerHTML
+    case 'text':
+      return element.textContent
+    case 'outerhtml':
+      return element.outerHTML
+    default:
+      return element.getAttribute(attr)
+  }
+}
+
+const querySelector = (selector, document) => {
+  const [token, content] = [selector[0], selector.slice(1)]
+  if (token === '.') return document.getElementsByClassName(content)
+  if (token === '^') return document.getElementsByTagName(content)
+  if (token === '~') return document.getElementsByName(content)
+  if (token === '#') return [document.getElementById(content)]
+  return [document.getElementById(selector)]
+}
+
+const present = items => console.log(items.join('\n'))
+
 module.exports = (async () => {
-  const [id, attribute] = args['_']
-  console.log(
-    new DomParser()
-    .parseFromString(await stdin())
-    .getElementById(id)
-    .getAttribute(attribute)
-  )
+  const [selector, attribute] = args['_']
+  const parsed = new DomParser().parseFromString(await stdin())
+  present(
+    querySelector(selector, parsed)
+    .map(getValue(attribute))
+    .filter(x => x))
 })()
